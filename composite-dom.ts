3b7a3b0533c7e0bbc8a178433b24685b40cba249
Makenzie Pletcher <mpletch24@gmail.edu>
2017-01-24 12:42:37 -0800

    lab4

--- composite-dom.ts
+++ composite-dom.ts
@@ -1,4 +1,53 @@
+
 interface IDomElement {
   print();
 }
 
+
+class TextNode implements IDomElement {
+  private text : string;
+  constructor(text : string) {
+    this.text = text;
+  }
+  print() {
+    console.log(this.text);
+  }
+}
+
+class DomElement implements IDomElement {
+  name : string;
+  content: string;
+  elements: IDomElement[];
+
+  constructor(elementName : string, textContent?: string) {
+    this.name = elementName;
+    this.elements = [];
+    this.content = textContent;
+  }
+
+  add(element: IDomElement) {
+    this.elements.push(element);
+  }
+
+  print() {
+    console.log(`<${this.name}>`);
+    if (this.content) {
+      console.log(this.content);
+    }
+    for (let element of this.elements) {
+      element.print();
+    }
+    console.log(`</${this.name}>`);
+  }
+}
+
+let p1 = new DomElement("p");
+let p2 = new DomElement("p");
+let div = new DomElement("div");
+let html = new DomElement("html");
+p1.add(new TextNode("Hi, I am a TextNode being printed!"));
+p2.add(new TextNode("TextNode == leaf"));
+div.add(p1);
+div.add(p2);
+html.add(div);
+html.print();
